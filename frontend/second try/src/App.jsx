e75f05327f4ffad54f91e305f0bebb5f6b264e1f
samay <samayverma44@gmail.com>
2025-05-18 22:18:51 +0530

    next

--- frontend/second try/src/App.jsx
+++ frontend/second try/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import navbar from './component/navbar'
 function App() {
   return (
     <div className='w-full h-screen bg-gray-800'>
-      
+      <navbar/>
     </div>
   )
 }
---
+++ frontend/second try/src/component/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+      <h1 className='text-red-700'>hello</h1>
+    </div>
+  )
+}
+
+export default navbar
